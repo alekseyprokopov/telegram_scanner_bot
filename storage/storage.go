907cd174326249fc97ab9f4dbcd641706cec3f5d
Prokopov Aleksey <prokopov.vrn@gmail.com>
2023-02-10 09:32:37 +0300

    fix storage methods, add string/config convert funcs

--- storage/storage.go
+++ storage/storage.go
@@ -1,38 +1,72 @@
 package storage
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Configuration struct {
-	UserName   string `json:"user_name"`
+	UserId     int `json:"user_name"`
 	UserConfig Config `json:"user_config"`
 }
 
 type Config struct {
-	payTypes  []string `json:"pay_types"`
-	minValue  int `json:"min_value"`
-	minSpread float64 `json:"min_spread"`
-	maxSpread float64 `json:"max_spread"`
-	platforms map[string]Platform `json:"platforms"`
+	PayTypes  []string            `json:"pay_types"`
+	MinValue  int                 `json:"min_value"`
+	MinSpread float64             `json:"min_spread"`
+	MaxSpread float64             `json:"max_spread"`
+	Platforms map[string]Platform `json:"platforms"`
 }
 
 type Platform struct {
-	IsActivePlatform bool `json:"is_active_platform"`
-	Roles    map[string]IsActiveRole `json:"roles"`
+	IsActivePlatform bool                    `json:"is_active_platform"`
+	Roles            map[string]IsActiveRole `json:"roles"`
 }
 
 type IsActiveRole bool
 
-var DefaultConfig = Config{
-	payTypes:  []string{"Sberbank", "Tinkoff", "QIWI", "YooMoney"},
-	minValue:  10000,
-	minSpread: 0.5,
-	maxSpread: 10,
-	platforms: map[string]Platform{
-		"binance": {
-			IsActivePlatform: true,
-			Roles: map[string]IsActiveRole{
-				"taker/taker": true,
-				"taker/maker":true,
-				"maker/taker":true,
-				"maker/maker":true,
+
+func UserConfigToString(c *Configuration) (userConfig string, err error) {
+	configuration := *c
+	s, err := json.Marshal(configuration.UserConfig)
+	if err != nil {
+		return "", fmt.Errorf("can't encode config: %w", err)
+	}
+	return string(s), nil
+}
+
+
+func StringToConfig(userID int, userConfig string) (*Configuration, error) {
+	pBytesUserConfig := []byte(userConfig)
+	var data Configuration
+	data.UserId = userID
+	if err := json.Unmarshal(pBytesUserConfig, &data.UserConfig); err != nil {
+		return nil, fmt.Errorf("can't transform string to Configuration: %w", err)
+	}
+
+	return &data, nil
+}
+
+func toDefaultConfig() *Configuration {
+	return &DefaultConfig
+}
+
+var DefaultConfig = Configuration{
+	UserId: 0,
+	UserConfig: Config{
+		PayTypes:  []string{"Sberbank", "Tinkoff", "QIWI", "YooMoney"},
+		MinValue:  10000,
+		MinSpread: 0.5,
+		MaxSpread: 10,
+		Platforms: map[string]Platform{
+			"binance": {
+				IsActivePlatform: true,
+				Roles: map[string]IsActiveRole{
+					"taker/taker": true,
+					"taker/maker": true,
+					"maker/taker": true,
+					"maker/maker": true,
+				},
 			},
 		},
 	},
